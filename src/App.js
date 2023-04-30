8a06c66bebc9e35a6bc0cf9d0792bb0233992e88
hansojin <jjaren746@github.com>
2023-04-30 22:13:44 +0900

    'state'

--- src/App.js
+++ src/App.js
@@ -5,12 +5,24 @@ import Content from "./components/Content"
 import Subject from "./components/Subject"
 
 class App extends Component{
+
+  constructor(props){   // state initalize
+    super(props);
+    this.state={
+      subject:{title:'WEB',sub:'World Wide Web'},
+      contents:[
+        {id:1, title:'HTML',desc:'for information'},
+        {id:2, title:'CSS',desc:'for design'},
+        {id:3, title:'JS',desc:'for interactive'}
+      ]
+    }
+  } 
+
   render(){
     return(
       <div className="App">
-        <Subject title="WEB" sub="World Wide Web"></Subject>
-        <Subject title="React" sub="for UI"></Subject>
-        <TOC></TOC>
+        <Subject title={this.state.subject.title} sub={this.state.subject.sub}></Subject>
+        <TOC data = {this.state.contents}></TOC>
         <Content title="HTML" desc="HTML is Hypertext Markup Language"></Content>
       </div>
     );
