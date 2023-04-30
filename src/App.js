1be76406e37f0beb75f24473c38f6289eac7d79d
hansojin <jjaren746@github.com>
2023-04-30 23:42:45 +0900

    'event'

--- src/App.js
+++ src/App.js
@@ -2,14 +2,16 @@ import './App.css';
 import React,{Component} from 'react';
 import TOC from "./components/TOC"
 import Content from "./components/Content"
-import Subject from "./components/Subject"
+// import Subject from "./components/Subject"
 
 class App extends Component{
 
   constructor(props){   // state initalize
     super(props);
     this.state={
+      mode:'welcome',
       subject:{title:'WEB',sub:'World Wide Web'},
+      welcome:{title:'Welcome',desc:'Hello React!'},
       contents:[
         {id:1, title:'HTML',desc:'for information'},
         {id:2, title:'CSS',desc:'for design'},
@@ -19,11 +21,33 @@ class App extends Component{
   } 
 
   render(){
+    var _title,_desc = null;
+    if (this.state.mode === 'welcome'){
+      _title=this.state.welcome.title;
+      _desc=this.state.welcome.desc;
+    }
+    else if (this.state.mode === 'read'){
+      _title=this.state.contents[0].title;
+      _desc=this.state.contents[0].desc;
+    }
     return(
       <div className="App">
-        <Subject title={this.state.subject.title} sub={this.state.subject.sub}></Subject>
+        {/* <Subject title={this.state.subject.title} sub={this.state.subject.sub}></Subject> */}
+        
+        <header>
+          <h1><a href="/" onClick={function(e){
+            console.log(e);
+            e.preventDefault();
+            // debugger;
+            this.setState({
+              mode:'welcome'
+            });
+          }.bind(this)}>{this.state.subject.title}</a></h1>
+          {this.state.subject.sub}
+        </header>
+
         <TOC data = {this.state.contents}></TOC>
-        <Content title="HTML" desc="HTML is Hypertext Markup Language"></Content>
+        <Content title={_title} desc={_desc}></Content>
       </div>
     );
   }
